d963f92141f1583fdcd53afdf56d5429713a9a46
saifa <saifalbayatti@gmail.com>
2024-07-29 20:54:08 +0100

    Project Set Up

--- src/main/java/com/app/commerce/controller/AdminDashboardController.java
+++ src/main/java/com/app/commerce/controller/AdminDashboardController.java
@@ -1,7 +1,14 @@
 package com.app.commerce.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class AdminDashboardController {
 
@@ -17,5 +24,13 @@ public class AdminDashboardController {
     Button viewOrdersBtn;
     @FXML
     Button adminLogOutBtn;
+
+    @FXML
+    public void adminLogOut() throws IOException {
+        Stage stage = (Stage) adminLogOutBtn.getScene().getWindow();
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/app/commerce/AdminLogin.fxml")));
+        stage.setScene(new Scene(root, 600, 400));
+        stage.setTitle("Admin Login");
+    }
 
 }
